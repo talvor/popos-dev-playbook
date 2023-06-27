d3fefc6733ac5b8e6701415d65a727134a5f4e84
Phillip Hall <talvor@me.com>
2023-06-27 22:08:04 +1000

    Fix dev container

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
-if ! command -v ansible &> /dev/null
-then
-    echo "**** Installing ansible"
-    sudo apt install python3-pip ansible
-    pip install --user ansible
-    export PATH=$PATH:~/.local/bin
-fi
+
+echo "**** Installing ansible"
+sudo apt install python3-pip ansible
+pip install --user ansible
+export PATH=$PATH:~/.local/bin
